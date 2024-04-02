7b1d1fe6813d968d429e78bba16a42d83d9faf8f
kosssshhhh <kosukwhon@gmail.com>
2024-04-02 16:46:05 +0900

    [ :sparkles: | 1 | Feat ] react query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
 						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg">
 						<path
-							clip-rule="evenodd"
-							fill-rule="evenodd"
+							clipRule="evenodd"
+							fillRule="evenodd"
 							d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
 					</svg>
 				</button>
@@ -58,9 +58,7 @@ function App() {
 										viewBox="0 0 18 21">
 										<path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
 									</svg>
-									<span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
-										E-commerce
-									</span>
+									<span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
 									<svg
 										className="w-3 h-3"
 										aria-hidden="true"
@@ -69,9 +67,9 @@ function App() {
 										viewBox="0 0 10 6">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="m1 1 4 4 4-4"
 										/>
 									</svg>
@@ -163,9 +161,7 @@ function App() {
 										viewBox="0 0 18 20">
 										<path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
 									</svg>
-									<span className="flex-1 ms-3 whitespace-nowrap">
-										Products
-									</span>
+									<span className="flex-1 ms-3 whitespace-nowrap">Products</span>
 								</a>
 							</li>
 							<li>
@@ -180,9 +176,9 @@ function App() {
 										viewBox="0 0 18 16">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
 										/>
 									</svg>
@@ -223,9 +219,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -241,9 +237,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -259,9 +255,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -278,9 +274,9 @@ function App() {
 									viewBox="0 0 18 18">
 									<path
 										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
 										d="M9 1v16M1 9h16"
 									/>
 								</svg>
@@ -297,9 +293,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -315,9 +311,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -333,9 +329,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -351,9 +347,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -370,9 +366,9 @@ function App() {
 									viewBox="0 0 18 18">
 									<path
 										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
 										d="M9 1v16M1 9h16"
 									/>
 								</svg>
@@ -389,9 +385,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -407,9 +403,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -425,9 +421,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
@@ -443,9 +439,9 @@ function App() {
 										viewBox="0 0 18 18">
 										<path
 											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M9 1v16M1 9h16"
 										/>
 									</svg>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from '@/App';
 import './index.css';
+import queryClient from '@/api/queryClient/queryClient';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+// TODO: React Query Devtools 오류 해결
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<QueryClientProvider client={queryClient}>
+		<App />
+		{/* <ReactQueryDevtools initialIsOpen={false} /> */}
+	</QueryClientProvider>,
+);

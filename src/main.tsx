95085a592700a042bff31b1b5ccf7e014acb34e9
kosssshhhh <kosukwhon@gmail.com>
2024-04-04 17:19:11 +0900

    [ :wrench: | 1 | Fix ] react-query devtools 오류 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from '@/App';
 import './index.css';
 import queryClient from '@/api/queryClient/queryClient';
 
-// TODO: React Query Devtools 오류 해결
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
 		<App />
-		{/* <ReactQueryDevtools initialIsOpen={false} /> */}
+		<ReactQueryDevtools initialIsOpen={false} />
 	</QueryClientProvider>,
 );

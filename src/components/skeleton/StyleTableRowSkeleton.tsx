40a02701fc4728521e6450da0fddf4f1e36611ff
kosssshhhh <kosukwhon@gmail.com>
2024-05-29 14:11:39 +0900

    [ :sparkles: | Feat ] TableRow Skeleton UI 구현

---
+++ src/components/skeleton/StyleTableRowSkeleton.tsx
@@ -0,0 +1,33 @@
+export default function StyleTableRowSkeleton() {
+	return (
+		<tr className="animate-pulse">
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 min-w-[100px] min-h-[120px] max-w-[100px] object-cover rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-20 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-20 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-24 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-16 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-16 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-24 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-24 mx-auto rounded"></div>
+			</td>
+			<td className="p-4 whitespace-nowrap text-center">
+				<div className="bg-gray-300 h-6 w-16 mx-auto rounded"></div>
+			</td>
+		</tr>
+	);
+}

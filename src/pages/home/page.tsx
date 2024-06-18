b9fcc127c79e5d522a16b91aaeb9a73210b0b1f5
kosssshhhh <kosukwhon@gmail.com>
2024-06-18 18:07:57 +0900

    BackEnd 연동 test

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,12 +1,4 @@
-import useNetwork from '@/stores/networkStore';
-
 export default function Home() {
-	const httpInterface = useNetwork((state) => state.httpInterface);
-
-	httpInterface.getMockData().then((res) => {
-		console.log(res);
-	});
-
 	return (
 		<>
 			<h1>Home Page</h1>
--- src/pages/styles/_hooks/useFetchStyles.ts
+++ src/pages/styles/_hooks/useFetchStyles.ts
@@ -7,11 +7,19 @@ import { useSearchParams } from 'react-router-dom';
 export const useFetchStyles = () => {
 	const [searchParams] = useSearchParams();
 
-	const [styles, setStyles] = useState<StyleType[]>([]);
+	// const [styles, setStyles] = useState<StyleType[]>([]);
 	const httpInterface = useNetwork((state) => state.httpInterface);
 
 	const fetchQuery = () => {
 		const params = new URLSearchParams(searchParams);
+
+		// page 파라미터를 1 빼서 설정
+		const page = params.get('page');
+		if (page) {
+			const adjustedPage = (parseInt(page, 10) - 1).toString();
+			params.set('page', adjustedPage);
+		}
+
 		console.log(params);
 
 		return params;
